dcdf6696797bdd5567d07f5ac1177ca62b795d5e
laurine <laurine.beduneau@epitech.eu>
2024-10-12 16:22:26 +0200

    auth improvement

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,15 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    // Récupérer le token depuis le header
     const token = req.header('x-auth-token');
 
-    // Vérifier si un token existe
     if (!token) {
         return res.status(401).json({ msg: 'Pas de token, autorisation refusée' });
     }
 
-    // Vérifier le token
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
