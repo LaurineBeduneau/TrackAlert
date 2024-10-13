3de8941fcbf0e0699b57680c90c4cd5333ef2f58
laurine <laurine.beduneau@epitech.eu>
2024-10-13 15:04:27 +0200

    ControllerImplementation of the integrated controllers and functional connection

---
+++ back/controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Inscription
+const registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'Utilisateur déjà existant' });
+        }
+
+        user = new User({
+            username,
+            email,
+            password: await bcrypt.hash(password, 10), // Chiffrer le mot de passe
+        });
+
+        await user.save();
+
+        const payload = { user: { id: user.id } };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(201).json({ token });
+    } catch (error) {
+        res.status(500).json({ msg: 'Erreur lors de l\'inscription' });
+    }
+};
+
+// Connexion
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Utilisateur non trouvé' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Mot de passe incorrect' });
+        }
+
+        const payload = { user: { id: user.id } };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ msg: 'Erreur lors de la connexion' });
+    }
+};
+
+module.exports = { registerUser, loginUser };
