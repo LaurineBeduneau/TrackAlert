33bfa1a5b5aa839b043b5c4224475de261ec8dc6
laurine <laurine.beduneau@epitech.eu>
2024-10-21 15:46:04 +0200

    user controller en amélioration (test)

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -34,21 +34,28 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ username });
+        console.log('Email reçu', email);
+        const user = await User.findOne({ email });
         if (!user) {
+            console.log('Utilisateur non trouvé')
             return res.status(400).json({ msg: 'Utilisateur non trouvé' });
         }
 
+        console.log('Utilisateur trouvé', user);
+
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
+            console.log('Mot de passe incorrect');
             return res.status(400).json({ msg: 'Mot de passe incorrect' });
         }
 
         const payload = { user: { id: user.id } };
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
+        console.log('Connexion réussie, token généré');
         res.json({ token });
     } catch (error) {
+        console.error('Erreur de connexion:',error)
         res.status(500).json({ msg: 'Erreur lors de la connexion' });
     }
 };
