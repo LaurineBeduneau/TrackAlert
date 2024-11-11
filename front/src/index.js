8acc1619a4ec4e0fa08aa91bd1012331523e4582
laurine <laurine.beduneau@epitech.eu>
2024-11-11 21:22:19 +0100

    Delete files of my app

--- front/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // Importer le createRoot de react-dom
-import App from '../App';
-
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
