6ed799511e8e761829f5deb07d5616153f9bfc8a
laurine <laurine.beduneau@epitech.eu>
2024-11-11 21:56:07 +0100

    Restauration du dossier front depuis un ancien commit

---
+++ front/src/navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import reportIncident from "../screens/ReportIncidentScreen";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Report" component={reportIncident} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigator;
